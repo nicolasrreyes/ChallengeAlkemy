6706eed2b29be0bf1b2ccb1cd3a03f5ebf90bacb
nicolasrreyes <nicolasrreyes@gmail.com>
2021-08-25 16:43:03 -0300

    add genre

--- src/main/java/com/challenge/controller/MovieRestController.java
+++ src/main/java/com/challenge/controller/MovieRestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.challenge.model.Movie;
-import com.challenge.model.Movie;
 import com.challenge.services.MovieService;
 
 @RestController
